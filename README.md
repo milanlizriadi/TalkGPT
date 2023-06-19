3366ac3e0334714982b2de86dee7a9302ff6aeef
Milanlizriadi <milanlizriadi@gmail.com>
2023-06-19 15:32:29 +0700

    TalkGPT

---
+++ README.md
@@ -0,0 +1,15 @@
+![TalkGPT](img/TalkGPT.png)
+
+<hr>
+
+## Warning
+You need to get your own api key and replace it in index.js file on line 49
+
+```js
+'X-RapidAPI-Key': 'Your Key',
+```
+
+<br>
+<br>
+
+![screenshot](img/screenshot.png)
---
+++ index.js
@@ -0,0 +1,101 @@
+const chatLog = document.getElementById("chat-log"),
+  userInput = document.getElementById("user-input"),
+  sendButton = document.getElementById("send-button"),
+  buttonIcon = document.getElementById("button-icon"),
+  info = document.querySelector(".info");
+
+sendButton.addEventListener("click", sendMessage);
+userInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    sendMessage();
+  }
+});
+
+function sendMessage() {
+  const message = userInput.value.trim();
+  // if message = empty do nothing
+  if (message === "") {
+    return;
+  }
+  // if message = developer - show our message
+  else if (message === "developer") {
+    // clear input value
+    userInput.value = "";
+    // append message as user - we will code it's function
+    appendMessage("user", message);
+    // sets a fake timeout that showing loading on send button
+    setTimeout(() => {
+      // send our message as bot(sender : bot)
+      appendMessage(
+        "bot",
+        "This Source Coded By Reza Mehdikhanlou \nYoutube : @AsmrProg"
+      );
+      // change button icon to default
+      buttonIcon.classList.add("fa-solid", "fa-paper-plane");
+      buttonIcon.classList.remove("fas", "fa-spinner", "fa-pulse");
+    }, 2000);
+    return;
+  }
+
+  // else if none of above
+  // appends users message to screen
+  appendMessage("user", message);
+  userInput.value = "";
+
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": "caafa44924mshd29951083634066p107487jsn08860bee0bce",
+      "X-RapidAPI-Host": "chatgpt53.p.rapidapi.com",
+    },
+    body: `{"messages":[{"role":"user","content":"${message}"}]}`,
+  };
+  fetch("https://chatgpt53.p.rapidapi.com/", options)
+    .then((response) => response.json())
+    .then((response) => {
+      appendMessage("bot", response.choices[0].message.content);
+
+      buttonIcon.classList.add("fa-solid", "fa-paper-plane");
+      buttonIcon.classList.remove("fas", "fa-spinner", "fa-pulse");
+    })
+    .catch((err) => {
+      if (err.name === "TypeError") {
+        appendMessage("bot", "Error : Check Your Api Key!");
+        buttonIcon.classList.add("fa-solid", "fa-paper-plane");
+        buttonIcon.classList.remove("fas", "fa-spinner", "fa-pulse");
+      }
+    });
+}
+
+function appendMessage(sender, message) {
+  info.style.display = "none";
+  // change send button icon to loading using fontawesome
+  buttonIcon.classList.remove("fa-solid", "fa-paper-plane");
+  buttonIcon.classList.add("fas", "fa-spinner", "fa-pulse");
+
+  const messageElement = document.createElement("div");
+  const iconElement = document.createElement("div");
+  const chatElement = document.createElement("div");
+  const icon = document.createElement("i");
+
+  chatElement.classList.add("chat-box");
+  iconElement.classList.add("icon");
+  messageElement.classList.add(sender);
+  messageElement.innerText = message;
+
+  // add icons depending on who send message bot or user
+  if (sender === "user") {
+    icon.classList.add("fa-regular", "fa-user");
+    iconElement.setAttribute("id", "user-icon");
+  } else {
+    icon.classList.add("fa-solid", "fa-robot");
+    iconElement.setAttribute("id", "bot-icon");
+  }
+
+  iconElement.appendChild(icon);
+  chatElement.appendChild(iconElement);
+  chatElement.appendChild(messageElement);
+  chatLog.appendChild(chatElement);
+  chatLog.scrollTo = chatLog.scrollHeight;
+}
